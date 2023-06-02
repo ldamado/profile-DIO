af33f43171661648c79e4b31076cd63274521594
Lucas Diniz Amado <130322564+ldamado@users.noreply.github.com>
2023-06-02 16:25:58 -0300

    Update api.js

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/ldamado/profile-clean/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/ldamado/profile-DIO/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
